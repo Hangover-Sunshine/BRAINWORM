f49c3c173568dc002dc6224d9a496d8cdfb0c8ae
SBrianZ <stevenbrianzavala@gmail.com>
2025-02-14 18:42:03 -0800

    The Big Menu Push 1
    - Added brand fonts and potential game font.
    - Integrated fonts into all menus.
    - Added company logo.
    - Created theme containing design for buttons and sliders.
    - Implemented placeholder pregame system for game instructions.

--- scripts/menus/menu_pregame.gd
+++ scripts/menus/menu_pregame.gd
@@ -3,8 +3,25 @@ extends Control
 signal pregame_to_game
 signal pregame_to_main
 
+@onready var pg = $Pregame_MC/Pregame_Sheet
+
 func _on_tutorial_button_pressed():
 	pregame_to_game.emit()
 
 func _on_back_button_pressed():
 	pregame_to_main.emit()
+
+func _on_skip_button_pressed():
+	pg.frame = pg.sprite_frames.get_frame_count("default") - 1
+
+func _on_increase_button_pressed():
+	if pg.frame != pg.sprite_frames.get_frame_count("default") - 1:
+		pg.frame += 1
+	else:
+		pg.frame = 0
+
+func _on_decrease_button_pressed():
+	if pg.frame != 0:
+		pg.frame -= 1
+	else:
+		pg.frame = pg.sprite_frames.get_frame_count("default") - 1
