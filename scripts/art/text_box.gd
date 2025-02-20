224a318d7ea993c249104b3905790d665ae01d62
SBrianZ <stevenbrianzavala@gmail.com>
2025-02-20 00:01:31 -0800

    Politician saying stupid shit
    - Spaghetti coded the politician cycling through 40 unique dumb quips on neuron consumption (in prog: needs to be connected to gameplay)

--- scripts/art/text_box.gd
+++ scripts/art/text_box.gd
@@ -1,6 +1,30 @@
-extends MarginContainer
+extends Node2D
 
-@onready var label = $MarginContainer/Label
-@onready var timer = $LetterDisplayTimer
+var game_line1: Array = ["Debtor's prisons ","Vaccines are made of... ","POGOSTICKS are ",
+"MY EARTH IS A","The POPSICLES","Trickle-down economics ","WIND TURBINES","Our water is making",
+"Has anyone seen my","Have you tried","BARK! BARK! BARK!","The internet is a",
+"Our SECRET SERVICE","A SQUIRREL CABAL","WHAT'S your RECIPE","Socialists are in","meow...purr...",
+"The IRS is a FRONT","Social security is a","We can make","Gravity was","Daylight SAVINGS is",
+"OUR ocean was","BIRDS aren't REAL!","The moon is","I HAVE A WORM","Anarchists are",
+"OUR constitution","The NATIONAL debt","Popular VOTING is","BIG medicaid","COMMUNISM hates",
+"Bubonic PLAGUE was","The HURRICANE","BEEP BOP BOOP!","Error 404:","You've got mail!",
+"A dozen EGGS","WELFARE is"]
 
-const MAX_WIDTH = 0
+var game_line2: Array = ["are a-okay.","GREEN BEANS!","COMMUNIST!","FLYING TORTOISE!",
+"HAVE fluoride.","work actually.","GIVE CANCER!","FROGS KISS!","CAR KEYS?","HORSE TRANQUILIZER?",
+"_","SERIES OF TUBES!","is a UNICORN!","stole the ELECTION.","for PECAN PIE?","MY SALAD!",">:3",
+"for SECRET PIZZA ","SCAM from THE ","NUKES from ","INVENTED by","a NASA PLOT for ",
+"A BOWL of SOUP ","They're DRONES!","CHEESE, however ","IN MY BRAIN!","MERMAIDS in ","was WRITTEN for ",
+"is A PIGGY BANK.","RIGGED from ","WILL sell ","TACO tuesday?","DEEP STATE ","was EDUCATED by ",
+"_","User not found.",":D","are BIG PHARMA!","theft FOR the"]
+
+var game_line3: Array = ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_",
+"DELIVERY!","LIZARDS!","BANANAS!","BIG SHOE!","potluck?","for GIANTS!","_","IT'S VEGAN!","_",
+"disguise!","SPOOKY GHOSTS.","GIMME hammer!","MUMMIES?","TOASTERS!","_","propaganda!",
+"public SCHOOLS!","_","_","_","_","MILKMAN?"]
+
+var cutscene_script: Array = ["My fellow Americans!","It is with great honor that I...",
+"Jerry Atrick...","Senator from the great state of...","Red White and Blue Land...",
+"announce my bid...","for U.S. Presidency!","The current administration...",
+"needs new legislation...","and I am your best hope...","to making this country...",
+"oh-so wonderful again."]
