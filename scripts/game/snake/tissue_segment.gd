9ae16a4598a7da7ed263d6785c59ddc690647a36
Mica Gardone <michaelgardone@gmail.com>
2025-02-21 14:38:56 -0700

    reoragnized tissue segment to have Node2D as root and Sprite2D as a child of that

--- scripts/game/snake/tissue_segment.gd
+++ scripts/game/snake/tissue_segment.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 
 var texture_rect:Rect2
 
@@ -8,22 +8,22 @@ func play_grow_animation(rect:Rect2):
 	# Once your animation is done, you have the tile rect stuff here
 	
 	# TODO: REPLACE THIS WITH AP CALL!
-	$AP.play("Spawn")
+	$TissueSegment/AP.play("Spawn")
 ##
 
 # NOTE: Brian -- in your animation player, call this function once you're done!
 func swap_to_actual():
-	region_rect = texture_rect
+	$TissueSegment.region_rect = texture_rect
 ##
 
 # NOTE: Brian -- Don't worry about this one, I need it for updating tiles.
 func change_texture_to(rect:Rect2):
-	region_rect = rect
+	$TissueSegment.region_rect = rect
 ##
 
 func kill():
 	# NOTE: You're responsible for cleaning this segment up
 	#	The tissue parent has already forgotten you by this point.
 	# TODO: REPLACE THIS WITH AN ANIM, THEN QUEUE FREE
-	$AP.play("Despawn")
+	$TissueSegment/AP.play("Despawn")
 ##
