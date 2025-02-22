29d001d3c2cf5aabee0f1108bbab7110217aafe5
SBrianZ <stevenbrianzavala@gmail.com>
2025-02-22 14:27:13 -0800

    GUI_Health Flashing, white blood cell fx
    - Added whiteblood cells to drop from frame on shake
    - Made GUI flash at different rates depending on stability level.

--- scripts/art/gui_health.gd
+++ scripts/art/gui_health.gd
@@ -9,4 +9,10 @@ func set_healthbar_value(value:int):
 
 func _on_gui_health_bar_value_changed(value):
 	number.text = str(value) +"%"
-
+	if value <= 75 and (value > 50):
+		$AP_Flash.play("75%")
+	elif value <= 50 and (value > 25):
+		$AP_Flash.play("50%")
+	elif value <= 25:
+		$AP_Flash.play("50%")
+##
